aa8926c2f4c37d3e444f2fa19c1c201956ec8d42
Brandon Duffany <brandonduffany@gmail.com>
2020-04-16 17:43:11 -0400

    Add Create-GitHubRepo script

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,3 +6,15 @@ Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1" -Force
 
 # Alias vim to nvim
 Function vim { nvim $args }
+
+# Initialize a directory as a Git repo and push it to GitHub.
+# The GitHub repo name is set to the current directory name.
+Function Create-GitHubRepo {
+  git init
+  git add .
+  git commit -m "Initial commit"
+  gh repo create
+  $dirname = (Get-Location).path | Split-Path -Leaf
+  git remote add origin "https://github.com/$env:GitHubUserName/$dirname.git"
+  git push -u origin master
+}
