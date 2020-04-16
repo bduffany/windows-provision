1aaf9fe1d09c8055ede20a4037388773abfd1ac9
Brandon Duffany <brandonduffany@gmail.com>
2020-04-16 10:11:33 -0400

    Use wincred as credential helper

--- windows_provision.ps1
+++ windows_provision.ps1
@@ -47,12 +47,15 @@ if ($name) {
 if ($email) {
   git config --global user.email "$email"
 }
+
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.last 'log -1 HEAD'
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.cm commit
 git config --global alias.st status
+
+git config --global credential.helper wincred
 
 # Install useful Python modules
 python3 -m pip install selenium joblib pandas numpy
